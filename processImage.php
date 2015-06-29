e5011b86200847991f22be09f0fb2e7549cd6338
Daniel Carlos de Souza <daniel.carlos.souza@gmail.com>
2015-06-29 14:29:32 -0300

    First commit

---
+++ processImage.php
@@ -0,0 +1,37 @@
+<?php
+//error_reporting(6135);
+
+if ($_SERVER["REQUEST_METHOD"]== "POST")
+{
+    if (isset($_POST["imagem"]))
+    {
+		$imagem =$_POST["imagem"];
+    }
+
+
+	
+	
+	if ($imagem != null && $imagem !="undefined")
+	{
+		if ($_POST["tipoImagem"]=="exibir")
+		{
+			ob_start();
+				header('Content-Length: ' . strlen($imagem));
+				header('Content-Type: image/jpeg');
+				print(base64_decode($imagem));
+
+			ob_end_flush();
+		}
+		else
+		{
+			$data = base64_decode($imagem);
+			$file = "img/" . uniqid() . '.jpg';
+			$success = file_put_contents($file, $data);
+			echo "Arquivo criado <a href='".$file."'>aqui</a>";
+		}
+	}
+
+}	
+?>
+
+
